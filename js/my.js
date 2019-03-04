2c4adea8ee1ce0c7e80a0c6ff775f19c91de8ded
1TrafalgarJR <rinozza@yandex.ru>
2019-03-04 20:54:40 +0300

    fix

--- js/my.js
+++ js/my.js
@@ -1,13 +1,41 @@
-let submitForm = function (name, position) {
+let submitForm = function (name, surname, floor, position) {
     let submitContainer = document.querySelector('#submit-container');
     submitContainer.style.display = 'none';
 
-    if (name !== '' || position !== '') {
+    if (name !== '' || surname !== '' || floor !== '' || position !== '') {
+        let resultContainer = submitContainer.querySelector('#result-container');
         let nameHTML = alertBlock();
+        let surnameHTML = alertBlock();
+        let floorHTML = alertBlock();
         let positionHTML = alertBlock();
+
+        resultContainer.classList.remove('alert-danger');
+        resultContainer.classList.remove('alert-primary');
 
         if (name !== '') {
             nameHTML = alertBlock('Имя: ' + name);
+
+        }
+
+        if (surname !== '') {
+            surnameHTML = alertBlock('Фамилия: ' + surname);
+        }
+
+        if (floor !== '') {
+            floorHTML = alertBlock('Пол: ' + floor);
+
+            switch (floor) {
+                case 'Женский':
+                    resultContainer.classList.add('alert-danger');
+
+                    break;
+                case 'Мужской':
+                    resultContainer.classList.add('alert-primary');
+
+                    break;
+                default:
+                    resultContainer.classList.add('alert-primary');
+            }
         }
 
         if (position !== '') {
@@ -15,8 +43,11 @@ let submitForm = function (name, position) {
         }
 
         submitContainer.querySelector('.js-name').innerHTML = nameHTML;
+        submitContainer.querySelector('.js-surname').innerHTML = surnameHTML;
+        submitContainer.querySelector('.js-floor').innerHTML = floorHTML;
         submitContainer.querySelector('.js-position').innerHTML = positionHTML;
         submitContainer.style.display = 'block';
+
     }
 };
 
